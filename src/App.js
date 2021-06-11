f0861539e7bc582ef5e0f817d1e0886f9c96d940
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-11 18:17:02 +0430

    change App.js from class base to functional base and add useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import { Component } from 'react'
+import { useState } from 'react';
 import './App.css';
 import Card from "./Card";
 import Thumbnail from "./Thumbnail";
@@ -15,41 +16,71 @@ import Thumbnail from "./Thumbnail";
 //     </div>
 //   );
 // }
+// ==========================
+// class App extends Component{
+//     state = {
+//         articles : [
+//             { id : 1 , title : 'article 1' , body : 'This is article 1'},
+//             { id : 2 , title : 'article 2' , body : 'This is article 2'},
+//             { id : 3 , title : 'article 3' , body : 'This is article 3'},
+//             { id : 4 , title : 'article 4' , body : 'This is article 4'},
+//         ]
+//     }
+//
+//     constructor() {
+//         super();
+//         setTimeout(() => {
+//             this.setState({
+//                 articles : [
+//                     { id : 5 , title : 'article 5' , body : 'This is article 5'},
+//                     { id : 6 , title : 'article 6' , body : 'This is article 6'},
+//                     { id : 7 , title : 'article 7' , body : 'This is article 7'},
+//                     { id : 8 , title : 'article 8' , body : 'This is article 8'},
+//                 ]
+//             })
+//         }, 2000);
+//     }
+//     render() {
+//         return (
+//             <div className="App">
+//               <Card title={this.state.articles[0].title} body={this.state.articles[0].body} />
+//               <Card title={this.state.articles[1].title} body={this.state.articles[1].body} />
+//               <Card title={this.state.articles[2].title} body={this.state.articles[2].body} />
+//               <Card title={this.state.articles[3].title} body={this.state.articles[3].body} />
+//               <Thumbnail title="x" body="y" />
+//             </div>
+//         );
+//     }
+// }
+// =========================
 
-class App extends Component{
-    state = {
+
+function App() {
+    const [stateArticle , setArticleState] = useState({
         articles : [
-            { id : 1 , title : 'article 1' , body : 'This is article 1'},
-            { id : 2 , title : 'article 2' , body : 'This is article 2'},
-            { id : 3 , title : 'article 3' , body : 'This is article 3'},
-            { id : 4 , title : 'article 4' , body : 'This is article 4'},
+            { id : 1 , title : 'article 1' , body : 'this is article 1'},
+            { id : 2 , title : 'article 2' , body : 'this is article 2'},
+            { id : 3 , title : 'article 3' , body : 'this is article 3'},
         ]
-    }
+    })
 
-    constructor() {
-        super();
-        setTimeout(() => {
-            this.setState({
-                articles : [
-                    { id : 5 , title : 'article 5' , body : 'This is article 5'},
-                    { id : 6 , title : 'article 6' , body : 'This is article 6'},
-                    { id : 7 , title : 'article 7' , body : 'This is article 7'},
-                    { id : 8 , title : 'article 8' , body : 'This is article 8'},
-                ]
-            })
-        }, 2000);
-    }
-    render() {
-        return (
-            <div className="App">
-              <Card title={this.state.articles[0].title} body={this.state.articles[0].body} />
-              <Card title={this.state.articles[1].title} body={this.state.articles[1].body} />
-              <Card title={this.state.articles[2].title} body={this.state.articles[2].body} />
-              <Card title={this.state.articles[3].title} body={this.state.articles[3].body} />
-              <Thumbnail title="x" body="y" />
-            </div>
-        );
-    }
+    setTimeout(() => {
+        setArticleState({
+            articles : [
+                { id : 4 , title : 'article 4' , body : 'this is article 4'},
+                { id : 5 , title : 'article 5' , body : 'this is article 5'},
+                { id : 6 , title : 'article 6' , body : 'this is article 6'},
+            ]
+        })
+    }, 2000);
+
+    return (
+        <div className="App">
+            {
+                stateArticle.articles.map(article => <Card title={article.title} body={article.body} />)
+            }
+        </div>
+    );
 }
 
 export default App;
