00e5239f87221ed09812c5fce6efb720316b8399
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-11 20:51:10 +0430

    add if statement in component

--- src/App.js
+++ src/App.js
@@ -16,50 +16,7 @@ import Thumbnail from "./Thumbnail";
 //     </div>
 //   );
 // }
-// ==========================
-class App extends Component{
-    state = {
-        articles : [
-            { id : 1 , title : 'article 1' , body : 'This is article 1'},
-            { id : 2 , title : 'article 2' , body : 'This is article 2'},
-            { id : 3 , title : 'article 3' , body : 'This is article 3'},
-            { id : 4 , title : 'article 4' , body : 'This is article 4'},
-        ]
-    }
 
-
-
-       loadMore = () => {
-           let article = [
-                { id : 5 , title : 'article 5' , body : 'this is article 5'},
-                { id : 6 , title : 'article 6' , body : 'this is article 6'},
-                { id : 7 , title : 'article 7' , body : 'this is article 7'},
-                { id : 8 , title : 'article 8' , body : 'this is article 8'},
-            ]
-
-        this.setState(prevSate => {
-            return {
-                articles: [ ... prevSate.articles , ... article]
-            }
-        })
-       }
-
-    render() {
-        let articleList = this.state.articles.map(article => <Card key={article.id} title={article.title} body={article.body} />)
-        return (
-            <div className="App">
-            {
-                articleList
-                // stateArticle.articles.map((article , index) => <Card key={index} title={article.title} body={article.body} />)
-            }
-            <Thumbnail title="x" body="y" />
-
-            <button onClick={this.loadMore}>load more</button>
-
-        </div>
-        );
-    }
-}
 // =========================
 
 
@@ -104,5 +61,63 @@ class App extends Component{
 //         </div>
 //     );
 // }
+
+
+
+// ==========================
+class App extends Component{
+    state = {
+        articles : [
+            { id : 1 , title : 'article 1' , body : 'This is article 1'},
+            { id : 2 , title : 'article 2' , body : 'This is article 2'},
+            { id : 3 , title : 'article 3' , body : 'This is article 3'},
+            { id : 4 , title : 'article 4' , body : 'This is article 4'},
+        ],
+        loading : false
+    }
+
+
+
+    loadMore = () => {
+        this.setState({ loading : true })
+        setTimeout(() => {
+            let article = [
+                { id : 5 , title : 'article 5' , body : 'this is article 5'},
+                { id : 6 , title : 'article 6' , body : 'this is article 6'},
+                { id : 7 , title : 'article 7' , body : 'this is article 7'},
+                { id : 8 , title : 'article 8' , body : 'this is article 8'},
+            ]
+
+            this.setState(prevSate => {
+                return {
+                    articles : [ ... prevSate.articles , ... article],
+                    loading : false
+                }
+            })
+        }, 3000)
+    }
+
+    render() {
+        let articleList = this.state.articles.map(article => <Card key={article.id} title={article.title} body={article.body} />)
+        return (
+            <div className="App">
+                { articleList }
+                {
+                    this.state.loading
+                        ? <div>Loading ...</div>
+                        : null
+                    // stateArticle.articles.map((article , index) => <Card key={index} title={article.title} body={article.body} />)
+                }
+                {/*<Thumbnail title="x" body="y" />*/}
+
+
+                <button onClick={this.loadMore}>load more</button>
+
+            </div>
+        );
+    }
+}
+
+
 
 export default App;
