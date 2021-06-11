5ad76d7b5d48fd6b06d798fdbfb1ac440a47a417
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-11 20:34:51 +0430

    change state in class base component

--- src/App.js
+++ src/App.js
@@ -17,82 +17,92 @@ import Thumbnail from "./Thumbnail";
 //   );
 // }
 // ==========================
-// class App extends Component{
-//     state = {
-//         articles : [
-//             { id : 1 , title : 'article 1' , body : 'This is article 1'},
-//             { id : 2 , title : 'article 2' , body : 'This is article 2'},
-//             { id : 3 , title : 'article 3' , body : 'This is article 3'},
-//             { id : 4 , title : 'article 4' , body : 'This is article 4'},
-//         ]
-//     }
-//
-//     constructor() {
-//         super();
-//         setTimeout(() => {
-//             this.setState({
-//                 articles : [
-//                     { id : 5 , title : 'article 5' , body : 'This is article 5'},
-//                     { id : 6 , title : 'article 6' , body : 'This is article 6'},
-//                     { id : 7 , title : 'article 7' , body : 'This is article 7'},
-//                     { id : 8 , title : 'article 8' , body : 'This is article 8'},
-//                 ]
-//             })
-//         }, 2000);
-//     }
-//     render() {
-//         return (
-//             <div className="App">
-//               <Card title={this.state.articles[0].title} body={this.state.articles[0].body} />
-//               <Card title={this.state.articles[1].title} body={this.state.articles[1].body} />
-//               <Card title={this.state.articles[2].title} body={this.state.articles[2].body} />
-//               <Card title={this.state.articles[3].title} body={this.state.articles[3].body} />
-//               <Thumbnail title="x" body="y" />
-//             </div>
-//         );
-//     }
-// }
-// =========================
-
-
-function App() {
-    const [stateArticle , setArticleState] = useState({
+class App extends Component{
+    state = {
         articles : [
-            { id : 1 , title : 'article 1' , body : 'this is article 1'},
-            { id : 2 , title : 'article 2' , body : 'this is article 2'},
-            { id : 3 , title : 'article 3' , body : 'this is article 3'},
-            { id : 4 , title : 'article 4' , body : 'this is article 4'},
+            { id : 1 , title : 'article 1' , body : 'This is article 1'},
+            { id : 2 , title : 'article 2' , body : 'This is article 2'},
+            { id : 3 , title : 'article 3' , body : 'This is article 3'},
+            { id : 4 , title : 'article 4' , body : 'This is article 4'},
         ]
-    })
-
-
-
-    let articleList = stateArticle.articles.map(article => <Card key={article.id} title={article.title} body={article.body} />)
-    const loadMore = (e) => {
-        let article = [
-            { id : 5 , title : 'article 5' , body : 'this is article 5'},
-            { id : 6 , title : 'article 6' , body : 'this is article 6'},
-            { id : 7 , title : 'article 7' , body : 'this is article 7'},
-            { id : 8 , title : 'article 8' , body : 'this is article 8'},
-        ]
-
-        setArticleState({
-            articles: [ ... stateArticle.articles , ... article]
-        })
     }
 
-    return (
-        <div className="App">
+
+
+       loadMore = () => {
+           let article = [
+                { id : 5 , title : 'article 5' , body : 'this is article 5'},
+                { id : 6 , title : 'article 6' , body : 'this is article 6'},
+                { id : 7 , title : 'article 7' , body : 'this is article 7'},
+                { id : 8 , title : 'article 8' , body : 'this is article 8'},
+            ]
+
+        this.setState(prevSate => {
+            return {
+                articles: [ ... prevSate.articles , ... article]
+            }
+        })
+       }
+
+    render() {
+        let articleList = this.state.articles.map(article => <Card key={article.id} title={article.title} body={article.body} />)
+        return (
+            <div className="App">
             {
                 articleList
                 // stateArticle.articles.map((article , index) => <Card key={index} title={article.title} body={article.body} />)
             }
             <Thumbnail title="x" body="y" />
 
-            <button onClick={loadMore}>load more</button>
+            <button onClick={this.loadMore}>load more</button>
 
         </div>
-    );
+        );
+    }
 }
+// =========================
+
+
+// function App() {
+//     const [stateArticle , setArticleState] = useState({
+//         articles : [
+//             { id : 1 , title : 'article 1' , body : 'this is article 1'},
+//             { id : 2 , title : 'article 2' , body : 'this is article 2'},
+//             { id : 3 , title : 'article 3' , body : 'this is article 3'},
+//             { id : 4 , title : 'article 4' , body : 'this is article 4'},
+//         ]
+//     })
+//
+//
+//
+//     let articleList = stateArticle.articles.map(article => <Card key={article.id} title={article.title} body={article.body} />)
+//     const loadMore = (e) => {
+//         let article = [
+//             { id : 5 , title : 'article 5' , body : 'this is article 5'},
+//             { id : 6 , title : 'article 6' , body : 'this is article 6'},
+//             { id : 7 , title : 'article 7' , body : 'this is article 7'},
+//             { id : 8 , title : 'article 8' , body : 'this is article 8'},
+//         ]
+//
+//         setArticleState( prevSate => {
+//             return {
+//                 articles: [ ...prevSate.articles , ...article]
+//             }
+//         })
+//     }
+//
+//     return (
+//         <div className="App">
+//             {
+//                 articleList
+//                 // stateArticle.articles.map((article , index) => <Card key={index} title={article.title} body={article.body} />)
+//             }
+//             <Thumbnail title="x" body="y" />
+//
+//             <button onClick={loadMore}>load more</button>
+//
+//         </div>
+//     );
+// }
 
 export default App;
