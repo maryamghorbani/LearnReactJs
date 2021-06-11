a8555549786ac946d380c7bc8d06f53aa91a1719
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-11 12:05:55 +0430

    create first functional component

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+
+
+function Card() {
+    return (
+        <div className="card">
+            <header className="card-header">
+                Header
+            </header>
+            <section className="card-content">
+                lorem lorem lorem lorem
+            </section>
+            <footer className="card-footer">
+                It's a footer
+            </footer>
+        </div>
+    )
+}
+
+export default Card;
