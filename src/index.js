77e1c6cd9cededb32e6c75d2681969da547dea1d
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-13 19:30:27 +0430

    add bootstrap to project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Card from "./components/Card";
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 
