f94d41ac75ea83bb49231dc3ac651365f1c28bc6
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-14 12:34:11 +0430

    modify error file

--- src/components/errors/ErrorBoundary.js
+++ src/components/errors/ErrorBoundary.js
@@ -4,12 +4,18 @@ class ErrorBoundary extends React.Component {
     state = {
         hasError : false
     }
+    static getDrivedStateFromError(error) {
+        return { hasError : true }
+    }
+    componentDidCatch(error, errorInfo) {
+        // send errorInfo to Server for store
+    }
 
     render() {
         if (this.state.hasError) {
-
+            return <h1>Errors!</h1>
         }
-        
+
         return this.props.children;
     }
 
